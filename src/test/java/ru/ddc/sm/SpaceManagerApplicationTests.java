bf7255d1b5b5af354de3c0b55a3d910c17dbbfae
Sergey Kovalev <dip.deep@mail.ru>
2022-12-30 12:09:03 +0300

    init commit

---
+++ src/test/java/ru/ddc/sm/SpaceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
